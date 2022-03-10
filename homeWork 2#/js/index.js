5ac62d47ff8e795311ae1ddd079e615d1c0bc932
Vadikvdm <vadikvdm@gmail.com>
2022-03-10 14:55:49 +0200

    Modif

---
+++ homeWork 2#/js/index.js
@@ -0,0 +1,36 @@
+let meaningN;
+let meaningM;
+
+do {
+    meaningN = +prompt("Введите значение N", 1)
+} while (!Number (meaningN > 0))
+
+alert(`Является ли число целым? = ${Number.isInteger(meaningN)}`);
+console.log(meaningN);
+
+do {
+    meaningM = +prompt("Введите значение M", 10)
+} while (!Number (meaningM > meaningN))
+
+alert(`Является ли число целым? = ${Number.isInteger(meaningM)}`);
+console.log(meaningM);
+
+let paritySign = confirm("Использовать парные числа?");
+console.log(paritySign);
+
+
+let sum = 0;
+
+if (paritySign) {
+    for (i = meaningN; i <= meaningM; i++) {
+            sum +=i;
+    }
+} else {
+    for (i = meaningN; i < meaningM; i++) {
+        if (i % 2 !== 0) {
+            sum +=i;   
+        }
+   console.log(i);
+    }
+}
+alert(`Результат суммы = ${sum}`);
